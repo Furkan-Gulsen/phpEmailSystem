80d28fe81eb811a30c260f03e0769743de504400
Buddha Nag <rahulnag514@gmail.com>
2019-07-18 10:08:55 +0530

    Added Remember me with cookies

--- loginPage/login.php
+++ loginPage/login.php
@@ -27,6 +27,17 @@ else{
 
     // Burada form'dan gelen kullanıcı adı ile mysql database'in içinde eşleştirme arıyoruz...
     if($username === $val['person_username']){
+		  if(!empty($_POST["remember_me"]))
+                {
+                $hour = time() + 3600 * 24 * 30;
+                setcookie('username', $username, $hour);
+                setcookie('password', $password, $hour);
+                }else{
+                  
+                        setcookie('username', "");
+                        setcookie('password', "");
+              
+                }
       if($password == $getControl['person_password']){
         $_SESSION['person'] = ['username' => $val['person_username'], 'email' => $val['person_email']];
         header('Location: index.php?page=mail');
@@ -45,9 +56,49 @@ else{
 
 
 <form id='form' action='' method='post' autocomplete="off">
+
+<?php 
+	
+	if(!empty($_COOKIE['username'])) {
+	
+	?>
+   <input type="text" name="username" value="<?php echo $_COOKIE['username'];?>" autocomplete="off" placeholder="username"> 
+<?php   
+}else{
+	
+?>
   <input type='text' name='username' autocomplete="off" placeholder='username'><br>
-  <input name="password" type="password" autocomplete="new-password" placeholder='password'><br>
+  <?php
+}
+?>
+<?php if(!empty($_COOKIE['password'])){
+
+	?>
+  <input name="password" type="password" value="<?php echo $_COOKIE['password'];?>" autocomplete="new-password" placeholder='password'><br>
+  <?php   
+}else{
+	
+?>
+  <input name="password" type="password"  autocomplete="new-password" placeholder='password'>
+  <?php
+}
+
+?>  
+
   <input type='hidden' value='1'/>
+     <label style="color:white;">
+	 <?php 	if(!empty($_COOKIE['username'])){
+		 ?>
+      <input type="checkbox" value="remember-me" name="remember_me" checked> Remember me
+	  <?php
+	 }else
+	 { 
+	 ?>
+	 <input type="checkbox" value="remember-me" name="remember_me" > Remember me
+	 <?php
+	 }	 
+	 ?>
+    </label>
   <button type='submit' name='submit'>LOGIN</button><br/>
   <a href='?page=homepage'>BACK</a>
 </form>
